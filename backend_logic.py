febae2df26260a1e105ba0103cece49650806511
Tarun rk <tarunrk2002@gmail.com>
2024-09-24 09:19:41 +0530

    ..

--- backend_logic.py
+++ backend_logic.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from base64 import b64decode
 import fitz
+import random
 
 class Item(BaseModel):
     pdf_base64: str
@@ -29,11 +30,13 @@ async def post_box(data: Item):
    pdf_64 = data.pdf_base64
    pdf_bytes = b64decode(pdf_64, validate=True)
    doc = fitz.open(stream=pdf_bytes, filetype="pdf")
+   random_x = random.randint(0, 100)
+   random_y = random.randint(0, 100)
    doc_info = {
         "file_size": len(pdf_bytes), 
         "page_count": doc.page_count,
         # "pdf_base64": pdf_64,
-        "bounding_box": {"x":150, "y":150, "width":150, "height":150},
+        "bounding_box": {"x":random_x, "y":random_y, "width":200, "height":30},
         "pg_num": 1
     }
    
