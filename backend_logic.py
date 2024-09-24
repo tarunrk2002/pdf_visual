aad550e8738fb108dbb1d6fa2ba0dcc99eb27eb0
Tarun rk <tarunrk2002@gmail.com>
2024-09-24 17:08:02 +0530

    countings

--- backend_logic.py
+++ backend_logic.py
@@ -34,6 +34,24 @@ async def post_box(data: Item):
    doc_width = doc[0].rect.width
    random_x = random.randint(0, 100)
    random_y = random.randint(0, 100)
+
+   ## counting the total number of lines and words in the document
+
+   line_count = 0
+   word_count = 0
+   for i in range(doc.page_count):
+       page = doc[i]
+
+    #counting for words
+       text_words = page.get_text("words")
+       word_count += len(text_words)
+
+    #counting for lines 
+       text_lines = page.get_text("text")
+       line_count += len(text_lines.split("\n"))
+
+
+
    doc_info = {
         "file_size": len(pdf_bytes), 
         "page_count": doc.page_count,
@@ -41,7 +59,11 @@ async def post_box(data: Item):
         "bounding_box": {"x":random_x, "y":random_y, "width":200, "height":30},
         "doc_height": doc_height,
         "doc_width": doc_width,
-        "pg_num": 1
+        "pg_num": 1,
+        "line_count": line_count,
+        
+        "word_count": word_count,
+        "text_from_pdf":text_lines,
     }
    
-   return {"message": "PDF created", "info": doc_info}
+   return {"info": doc_info}
